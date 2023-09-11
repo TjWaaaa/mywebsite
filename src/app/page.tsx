20aecdf467c821433d2ac18939351d4024d09c37
TjWaaaa <timowaldherr98@gmail.com>
2023-09-11 15:56:13 +0200

    feat: shrink headerbar on scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,30 +11,30 @@ import { HomeSections } from '@/types/reactTypes'
 
 export default function Home() {
 	const [activeSection, setActiveSection] = useState<HomeSections>(HomeSections.heroSection)
-	const [windowHeight, setWindowHeight] = useState<number>(0)
+	const [windowHeight, setWindowHeight] = useState<number>(5000)
 	const [scrollY, setScrollY] = useState<number>(0)
 	const sections = useRef<any>([])
 
-	const handleScroll = () => {
-		const pageYOffset = window.scrollY
-		setScrollY(pageYOffset)
-
-		let newActiveSection = HomeSections.heroSection
-
-		sections.current.forEach((section: any) => {
-			const sectionOffsetTop = section.offsetTop
-			const sectionHeight = section.offsetHeight
-
-			if (pageYOffset >= sectionOffsetTop && pageYOffset < sectionOffsetTop + sectionHeight) {
-				newActiveSection = section.id
-			}
-		})
-
-		setActiveSection(newActiveSection)
-	}
-
 	useEffect(() => {
 		setWindowHeight(window.innerHeight)
+
+		const handleScroll = () => {
+			const pageYOffset = window.scrollY
+			setScrollY(pageYOffset)
+
+			let newActiveSection = HomeSections.heroSection
+
+			sections.current.forEach((section: any) => {
+				const sectionOffsetTop = section.offsetTop
+				const sectionHeight = section.offsetHeight
+
+				if (pageYOffset >= sectionOffsetTop && pageYOffset < sectionOffsetTop + sectionHeight) {
+					newActiveSection = section.id
+				}
+			})
+
+			setActiveSection(newActiveSection)
+		}
 
 		sections.current = document.querySelectorAll('[data-section]')
 		window.addEventListener('scroll', handleScroll)
@@ -42,7 +42,7 @@ export default function Home() {
 		return () => {
 			window.removeEventListener('scroll', handleScroll)
 		}
-	}, [])
+	}, [scrollY])
 
 	return (
 		<main className="scroll-smooth">
