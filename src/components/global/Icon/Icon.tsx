f558cb17c1b1acf0fb5bf24f0e8ac5e18af942bb
TjWaaaa <timowaldherr98@gmail.com>
2023-09-13 16:42:56 +0200

    fix: get text size function moved into Icon

--- src/components/global/Icon/Icon.tsx
+++ src/components/global/Icon/Icon.tsx
@@ -1,4 +1,4 @@
-import { getFontSize, getTextColor } from '@/services/tailwindHelpers'
+import { getTextColor } from '@/services/tailwindHelpers'
 import { Colors, Sizes } from '@/types/tailwindTypes'
 import { MaterialSymbol } from 'material-symbols'
 
@@ -16,13 +16,44 @@ type IconProps = {
 /**
  * The `Icon` component is used to display a material icon.
  */
-const Icon: React.FC<IconProps> = ({ icon, isFilled = false, color = 'black', size = 'md' }) => (
-	<i
-		style={isFilled ? { fontVariationSettings: "'FILL' 1" } : {}}
-		className={`${getTextColor(color)} ${getFontSize(size)} material-symbols-rounded duration-700 ease-in-out`}
-	>
-		{icon}
-	</i>
-)
+const Icon: React.FC<IconProps> = ({ icon, isFilled = false, color = 'black', size = 'md' }) => {
+	const getFontSize = (size: Sizes) => {
+		switch (size) {
+			case 'xs':
+				return '!text-xs'
+			case 'sm':
+				return '!text-sm'
+			case 'md':
+				return '!text-base'
+			case 'lg':
+				return '!text-lg'
+			case 'xl':
+				return '!text-xl'
+			case '2xl':
+				return '!text-2xl'
+			case '3xl':
+				return '!text-3xl'
+			case '4xl':
+				return '!text-4xl'
+			case '5xl':
+				return '!text-5xl'
+			case '6xl':
+				return '!text-6xl'
+			case '7xl':
+				return '!text-7xl'
+			case '8xl':
+				return '!text-6xl sm:text-7xl lg:text-8xl'
+		}
+	}
+
+	return (
+		<i
+			style={isFilled ? { fontVariationSettings: "'FILL' 1" } : {}}
+			className={`${getTextColor(color)} ${getFontSize(size)} material-symbols-rounded duration-700 ease-in-out`}
+		>
+			{icon}
+		</i>
+	)
+}
 
 export default Icon
