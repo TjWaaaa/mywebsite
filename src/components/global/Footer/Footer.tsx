51d597dc71733d92efd473ed1de7f7de3da42c09
TjWaaaa <timowaldherr98@gmail.com>
2023-10-22 16:11:03 +0200

    feat: add imprint content

--- src/components/global/Footer/Footer.tsx
+++ src/components/global/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
 		<div className="w-full bg-main-200 py-6 border-t-2 border-solid border-white">
 			<div className="container md:pr-20 lg:pr-24 xl:pr-28 grid grid-cols-2 py-4 text-2xl font-semibold">
 				<div>
-					<p className="text-lg">Timo Walderr Stuttgart, Germany</p>
+					<p className="text-lg">Timo Walderr - Stuttgart, Germany</p>
 				</div>
 				<div className="m- flex justify-end">
 					<Button size="md" text="Imprint" color="transparent" hasPadding={false} href={routes.imprint} />
