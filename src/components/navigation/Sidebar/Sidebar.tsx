6a9316fdd45371b99a2e347be473cf67cc0f50e8
TjWaaaa <timowaldherr98@gmail.com>
2023-11-18 15:40:17 +0100

    fix: sidebar not clickable when invisible

--- src/components/navigation/Sidebar/Sidebar.tsx
+++ src/components/navigation/Sidebar/Sidebar.tsx
@@ -11,13 +11,14 @@ export type SidebarProps = {
 type ListItemProps = {
 	children: React.ReactNode
 	isActive: boolean
+	isClickable: boolean
 	href: string
 }
 
-const ListItem: React.FC<ListItemProps> = ({ children, isActive, href }) => {
+const ListItem: React.FC<ListItemProps> = ({ children, isActive, isClickable, href }) => {
 	return (
 		<a
-			href={href}
+			href={isClickable ? href : undefined}
 			className={`${
 				isActive && 'bg-main-400'
 			} rounded-lg w-12 h-12 flex justify-center items-center hover:bg-main-400 duration-300`}
@@ -30,17 +31,29 @@ const ListItem: React.FC<ListItemProps> = ({ children, isActive, href }) => {
 const Sidebar: React.FC<SidebarProps> = ({ activeSection = '', isVisible }) => {
 	return (
 		<div
-			className={`z-40 fixed flex flex-col items-center justify-between py-8 px-1 gap-3 bg-main-200 rounded-full right-4 lg:right-6 xl:right-8 2xl:right:10 top-1/3 drop-shadow-md font-bold duration-1000 ${
+			className={`z-40 fixed flex flex-col items-center justify-between py-8 px-1 gap-3 bg-main-200 rounded-full right-4 lg:right-6 xl:right-8 2xl:right:10 top-1/3 drop-shadow-md font-bold select-none duration-1000 ${
 				isVisible ? '!opacity-0 md:!opacity-100' : '!opacity-0'
 			}`}
 		>
-			<ListItem href={`#${HomeSections.heroSection}`} isActive={activeSection === HomeSections.heroSection}>
+			<ListItem
+				href={`#${HomeSections.heroSection}`}
+				isActive={activeSection === HomeSections.heroSection}
+				isClickable={isVisible}
+			>
 				<Icon icon="house" size="2xl" />
 			</ListItem>
-			<ListItem href={`#${HomeSections.experience}`} isActive={activeSection === HomeSections.experience}>
+			<ListItem
+				href={`#${HomeSections.experience}`}
+				isActive={activeSection === HomeSections.experience}
+				isClickable={isVisible}
+			>
 				<Icon icon="school" size="2xl" />
 			</ListItem>
-			<ListItem href={`#${HomeSections.projects}`} isActive={activeSection === HomeSections.projects}>
+			<ListItem
+				href={`#${HomeSections.projects}`}
+				isActive={activeSection === HomeSections.projects}
+				isClickable={isVisible}
+			>
 				<Icon icon="amp_stories" size="2xl" />
 			</ListItem>
 			{/* <ListItem href={`#${HomeSections.playground}`} isActive={activeSection === HomeSections.playground}>
